ae5f14bcf0423e2ce7b470f05255cf8c510f974f
Guillermo Segura <guillermo.segura@axity.com>
2020-07-14 15:22:11 -0500

    Se actualiza el proyecto

---
+++ src/main/java/com/axity/crud/controller/SampleController.java
@@ -0,0 +1,57 @@
+package com.axity.crud.controller;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import com.axity.crud.to.XmlUtil;
+
+@RestController
+@RequestMapping("/")
+public class SampleController {
+
+	@PostMapping(value = "/sample", consumes = MediaType.TEXT_PLAIN_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> save(@RequestBody String data) {
+		System.out.println(data);
+		return new ResponseEntity<>("OK", HttpStatus.OK);
+	}
+
+	@PostMapping(value = "/process", consumes = MediaType.TEXT_PLAIN_VALUE, produces = MediaType.TEXT_HTML_VALUE)
+	public ResponseEntity<String> process(@RequestBody String data) {
+
+		String[] parts = data.split("=");
+		System.out.println(parts[0]);
+
+		String response;
+		try {
+			String xml = URLDecoder.decode(parts[1], StandardCharsets.UTF_8.toString());
+			String msisdn = XmlUtil.getText(xml, "MSISDN");
+			if (msisdn != null) {
+				msisdn = StringUtils.replace(msisdn, "+52", "");
+			}
+			System.out.println("Enviar sms a " + msisdn);
+
+			response = "OK";
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			response = "NOK";
+		}
+
+		return new ResponseEntity<>(response, HttpStatus.OK);
+	}
+
+}
